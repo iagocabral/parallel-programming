58d370caf25e0c0a019c242bdc107a99202e6c9c
Iago Cabral <87732154+iagocabral@users.noreply.github.com>
2024-12-15 20:55:14 -0300

    prova

---
+++ OpenMp/textoRepetido.c
@@ -0,0 +1,33 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#define MAX 1000
+int main(void) {
+char texto_base[] = "abcdefghijklmnopqrstuvwxyz 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int *indice = (int *)malloc(sizeof(int));
+*indice = 0;
+struct timeval tv;
+int number, tmp_index, i, cont = 0;
+#pragma omp parallel for num_threads(3) private(tv, number, tmp_index, i)
+for (cont = 0; cont < MAX; cont++) {
+#pragma omp critical
+{
+gettimeofday(&tv, NULL);
+number = ((tv.tv_usec / 47) % 3) + 1;
+tmp_index = *indice;
+for (i = 0; i < number; i++)
+if (!(tmp_index + i > sizeof(texto_base)))
+
+fprintf(stderr,"%c", texto_base[tmp_index + i]);
+*indice = tmp_index + i;
+if (tmp_index + i > sizeof(texto_base)) {
+fprintf(stderr,"\n");
+*indice = 0;
+}
+}
+}
+printf("\n");
+free(indice);
+return 0;
+}
